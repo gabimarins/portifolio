622363b4a5ae5fa1e95d85b4862ec05905e41ef1
gabimarin <gabriela.santos9@estudante.ifms.edu.br>
2023-08-30 07:42:32 -0400

    my second commit

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,9 +1,12 @@
 import './Main.css'
 import Gabriela from "..//img/gabriela.jpeg"
+import Gabrielona from "..//img/gabrielona.jpeg"
 
 
 function Main(){
     return(
+        <div className="geral">
+        <div className="bloco1">
         <div className="main-content">
             <div className="text-gabriela">
                 <div className="texto">
@@ -13,14 +16,32 @@ function Main(){
                 ansiedade e reflexão para mim, pois eu ainda não sei que curso fazer. Enfim... eu gosto de ler livros de romance, fantasia<br></br>
                 e distopia. Também gosto muito de escrever e ler. Sou bolsista em um projeto da FUNDECT que visa estudar as Mudanças Climáticas <br></br>
                 e já tive um projeto publicado sobre aprendizado de máquina. Aqui, irei postar tudo o que é de meu interesse.
-                <br></br><br></br>Por hora é só, muito obrigada!</p>
+                <br></br><br></br>Continue aqui para saber mais!</p>
                 </div>
                 <img src={Gabriela} alt=""></img>
             </div>
+            </div>
 
-
-
+            <div className="bloco2">
+        <div className="main-content2">
+            <div className="text-gabriela2">
+            <img src={Gabrielona} alt=""></img>
+                <div className="texto2">
+                <h1>Alguns fatos sobre mim...</h1>
+                <p className="sinopse2">1 - Eu amo fazer interfaces de sites, seja no figma ou no vs code!<br></br>
+                2 - Durante a pandemia, no ano de 2021, li 100 livros! Finalizei o ano com a saga de Percy Jackson.<br></br>
+                3 - Amo fazer viagens, especialmente para a praia! <br></br>
+                4 - Eu uso óculos e já consegui quebrar os 3 que tive, então atualmente estou sem óculos nenhum... <br></br>
+                5 - Sou uma pessoa muito indecisa, desde um look até um curso da faculdade<br></br>
+                (Extra) Pedro disse que sou cú doce e recuso todos os contatinhos da vida. 
+                <br></br><br></br>Nos tópicos do rodapé você encontra mais informações sobre mim :)</p>
+                </div>
+                
+            </div>
+            </div>
             
+        </div>
+        </div>
         </div>
 
 
