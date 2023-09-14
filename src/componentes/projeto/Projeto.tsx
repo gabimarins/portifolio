358c754117322dba6620ff2c51b3a26846390cb6
gabimarins <141922950+gabimarins@users.noreply.github.com>
2023-09-14 09:53:37 -0400

    Update Projeto.tsx

--- src/componentes/projeto/Projeto.tsx
+++ src/componentes/projeto/Projeto.tsx
@@ -1,1 +1,30 @@
+//import imagem from "..//img/barbie.png"
+import './Projeto.css'
+type ProjetoProps = {
+    titulo: string,
+    sinopse:string,
+    link: string,
+    imagem:string
 
+}
+
+function Projeto(props:ProjetoProps){
+    return(
+        <div className="projeto-content">
+            {/* o comentario aqui é desse jeito, dentro das
+            chaves vc pode usar js, por isso da pra importar
+            o "imagem" dentro do componente IMG */}
+           <div className="foto_projeto">
+                <img className="img-projeto" src={props.imagem} alt=""></img>
+           </div> 
+
+            <div className="text-projetos">
+                <h1>{props.titulo}</h1>
+                <p className="sinopse">{props.sinopse}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Projeto
