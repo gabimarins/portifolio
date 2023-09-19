8e424044d390c9e9ca76ca00179435cc66adf716
gabimarin <gabriela.santos9@estudante.ifms.edu.br>
2023-09-19 10:16:06 -0400

    site quase pronto

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -20,14 +20,14 @@ export default function Main(){
          {
               id: 1,
               titulo:'SITE DA BARBIE',
-              sinopse:"Site com todos os filmes da Barbie, desenvolvido com o auxílio do professor Guilherme Terenciani. Uma curiosidade: o site da Barbie foi utilizado como base para a produção desse.",
+              sinopse:"Plataforma online com todos os filmes da Barbie, desenvolvido com o auxílio do professor Guilherme Terenciani. Uma curiosidade: o site da Barbie foi utilizado como base para a produção desse.",
               link: "Acesse aqui",
               imagem: "/barbie.png"
        },
         {
               id: 2,
               titulo:'NAVITRINE',
-              sinopse:"Plataforma online de exposição de produtos das lojas do comércio de moda e vestuário de Naviraí. Projeto desenvolvido em colaboração com meus companheiros: Camila Morais, Ellen Pinheiro e Pedro Ribeiro.",
+              sinopse:"Plataforma online de exposição de produtos das lojas do comércio de moda e vestuário de Naviraí. Projeto desenvolvido em colaboração com meus colegas de turma: Camila Morais, Ellen Pinheiro e Pedro Ribeiro.",
               link: "Acesse aqui",
               imagem: "/navitrine.png"
        },
@@ -38,12 +38,40 @@ export default function Main(){
               link: "Acesse aqui",
               imagem: "/lemais.png"
        },
-      {
+       {
               id: 4,
+              titulo:'CALENGA',
+              sinopse:"Plataforma onde pessoas com conhecimentos em diversos assuntos podem publicar seus tutoriais, para que outras possam aprender e começar a fazer a sua própria renda. Conteúdos disponibilizados de moda, estética, artesanato, edição e muito mais.",
+              link: "Acesse aqui",
+              imagem: "/calenga.png"
+       },
+      {
+              id: 5,
               titulo:'GAVECH',
-              sinopse:"Loja online de moda de grife. Projeto desenvolvido em parceria com os alunos: André Lacerda, Camila Morais, Ellen Pinheiro, Henrique Gavazzoni, Vitor Michelotto.",
+              sinopse:"Plataforma de loja online de moda de grife. Projeto desenvolvido em parceria com os alunos: Camila Morais, Ellen Pinheiro e Vitor Michelotto.",
               link: "Acesse aqui",
               imagem: "/gavech.png"
+       },
+       {
+              id: 6,
+              titulo:'Aroma de Café',
+              sinopse:"Plataforma para exposição, compra e venda de produtos da padaria Aroma de Café.",
+              link: "Acesse aqui",
+              imagem: "/cafe.png"
+       },
+       {
+              id: 7,
+              titulo:'Previsão de Produtividade de Soja no MS por Aprendizagem de Máquina',
+              sinopse:"Artigo científico traduzido para o inglês publicado na revista Springer. Utilizado como TCC. Premiado na FETECMS 2022 (2º) em Ciências Agrárias. Orientador: Guilherme Botega Torsoni. Coorientador: Lucas Aparecido.",
+              link: "Acesse aqui",
+              imagem: "/soja.png"
+       },
+       {
+              id: 7,
+              titulo:'Previsão de Produtividade de Soja no MS por Aprendizagem de Máquina',
+              sinopse:"Artigo científico traduzido para o inglês publicado na revista Springer. Utilizado como TCC. Premiado na FETECMS 2022 (2º) em Ciências Agrárias. Orientador: Guilherme Botega Torsoni. Coorientador: Lucas Aparecido.",
+              link: "Acesse aqui",
+              imagem: "/soja.png"
        }
     ]
 
@@ -54,35 +82,37 @@ export default function Main(){
     
     return(
     <>
-        {
-              <div className="campopesquisa"> 
-                     <div id="divBusca">
-                            <input type="text" className="botao_pesquisa" placeholder="O que deseja?" onChange={TrataTexto}/>
-                            <button className="divBusca"><img src={lupa}></img></button>
-                    </div>
-                    
-      <h1 className="titulo_projeto">Meus projetos:</h1>
-      {(texto)?<p>Resultados para: {texto}</p>:""}
-              </div>
-       }
-    
-    <div className="geral"> <div className="bloco1">
+
+<div className="geral"> 
+       
+<div className="bloco1">
                <div className="main-content">
                    <div className="text-gabriela">
                        <div className="texto">
                        <h1>Olá! Muito prazer, eu sou a Gabriela.</h1>
-                       <p className="sinopse">Me chamo Gabriela Marins dos Santos. Atualmente, sou estudante do 3º ano do Técnico em Informática<br></br>
-                       para Internet do IFMS Campus Naviraí. Atualmente, estou na correria com os estudos para o Enem. É um momento de muita <br></br>
-                       ansiedade e reflexão para mim, pois eu ainda não sei que curso fazer. Enfim... eu gosto de ler livros de romance, fantasia<br></br>
-                       e distopia. Também gosto muito de escrever e ler. Sou bolsista em um projeto da FUNDECT que visa estudar as Mudanças Climáticas <br></br>
-                       e já tive um projeto publicado sobre aprendizado de máquina. Aqui, irei postar tudo o que é de meu interesse.
+                       <p className="sinopse">Me chamo Gabriela Marins dos Santos. Atualmente, sou estudante do 3º ano do Técnico em Informática
+                       para Internet do IFMS Campus Naviraí. Ao longo da minha trajetória acadêmica, desenvolvi diversos projetos que incluem interface gráfica,
+                       programação e escrita científica. Adoro essa área e penso em levar adiante na minha vida. Nesse site, você poderá ter acesso ao meu portifólio,
+                       que conta com os principais conteúdos produzidos por mim até aqui. Espero que gostem. Abraços!
                        <br></br><br></br>Continue aqui para saber mais!</p>
                        </div>
-                       <img src={Gabriela} alt=""></img>
+                       {<div className="campopesquisa"> 
+                     <div id="divBusca">
+                            <input type="text" className="botao_pesquisa" placeholder="O que deseja encontrar?" onChange={TrataTexto}/>
+                            <button className="divBusca"><img src={lupa}></img></button>
+                    </div>
+              </div>}
+                     
                    </div>
-            </div></div>
-            
-        <main className="content-main">
+                   <img src={Gabriela} alt=""></img>
+            </div></div></div>
+
+        <div className='resultados'>
+        <h1 className="titulo_projeto">Projetos realizados por mim:</h1>
+            {(texto)?<p>Resultados para: {texto}</p>:""}
+        </div>
+           
+<main className="content-main">
                      {projetos.filter((projeto) => {
                      const textoSemEspacos = texto.replace(/\s/g, ''); 
                      const tituloSemEspacos = projeto.titulo.replace(/\s/g, '');
@@ -96,31 +126,8 @@ export default function Main(){
                      <Projeto key={projeto.id} titulo={projeto.titulo} sinopse={projeto.sinopse}  link={projeto.link} imagem={projeto.imagem} />
                      ))}
         </main>
-
-        
-        <div className="geral">
-               
-              
-
-            <div className="bloco2">
-               <div className="main-content2">
-                   <div className="text-gabriela2">
-                   <img src={Gabrielona} alt=""></img>
-                       <div className="texto2">
-                       <h1>Alguns fatos sobre mim...</h1>
-                       <p className="sinopse2">1 - Eu amo fazer interfaces de sites, seja no figma ou no vs code!<br></br>
-                       2 - Durante a pandemia, no ano de 2021, li 100 livros! Finalizei o ano com a saga de Percy Jackson.<br></br>
-                       3 - Amo fazer viagens, especialmente para a praia! <br></br>
-                       4 - Eu uso óculos e já consegui quebrar os 3 que tive, então atualmente estou sem óculos nenhum... <br></br>
-                       5 - Sou uma pessoa muito indecisa, desde um look até um curso da faculdade<br></br>
-                       (Extra) Pedro disse que sou cú doce e recuso todos os contatinhos da vida. 
-                       <br></br><br></br>Nos tópicos do rodapé você encontra mais informações sobre mim :)</p>
-                </div>
-                   </div>
-                   </div>
-                      </div>
-               </div>
-        </div>
+    
+     
 
 
      </>
