46599f5f2cc2b891d6c45be869217d11876510ee
gabimarin <gabriela.santos9@estudante.ifms.edu.br>
2023-10-20 11:00:39 -0400

    pronto

--- src/contato/Contato.tsx
+++ src/contato/Contato.tsx
@@ -5,8 +5,8 @@ import './Contato.css'
 
  function Contato(){
    const [Nome, setNome]=useState("")
-   const [Email, setEmail]=useState("")
-   const [Numero, setNumero]=useState("")
+  //  const [Email, setEmail]=useState("")
+  //  const [Numero, setNumero]=useState("")
 
 
     // function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
@@ -26,8 +26,8 @@ import './Contato.css'
       
       <div className="campodigitado">{Nome && <p>Olá {Nome}!</p>}</div>
       <div className="tituloinput">Nome:<input className="inputfooter nome" type="text" placeholder="Nome:" onChange={(e) => setNome(e.target.value)}/></div>
-      <div className="tituloinput">Email:<input className="inputfooter email" type="text" placeholder="Email para contato:" onChange={(e) => setEmail(e.target.value)}/></div>
-      <div className="tituloinput">Número:<input className="inputfooter email" type="tel" placeholder="Número para contato:" onChange={(e) => setNumero(e.target.value)}/></div>
+      <div className="tituloinput">Email:<input className="inputfooter email" type="text" placeholder="Email para contato:" /></div>
+      <div className="tituloinput">Número:<input className="inputfooter email" type="tel" placeholder="Número para contato:" /></div>
       <div className="tituloinput">Mensagem:<input className="inputfooter mensagem" type="text" placeholder="Deixe sua mensagem:"/></div>
       <button className="buttonfooter botaoenvio">Enviar mensagem</button>
     </div>
